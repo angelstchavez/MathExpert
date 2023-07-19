9236b5a8b00286623bb25e3f3b7a9ae22c4f75ec
angelstchavez <angelstchavez@gmail.com>
2023-07-19 02:01:02 -0500

    Mejorar pruebas en CustomerNUnitTests para el método GreetCustomer

--- MathExpert.NUnitTest/CustomerNUnitTests.cs
+++ MathExpert.NUnitTest/CustomerNUnitTests.cs
@@ -15,7 +15,12 @@ namespace MathExpert.NUnitTest
             string result = customer.GreetCustomer("Angel", "Chavez");
 
             //Assert
+            Assert.AreEqual(result, "Hello Angel Chavez");
             Assert.That(result, Is.EqualTo("Hello Angel Chavez"));
+            Assert.That(result, Does.Contain("Hello"));
+            Assert.That(result, Does.StartWith("Hello"));
+            Assert.That(result, Does.EndWith("Chavez"));
+            Assert.That(result, Does.Match("Hello [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
         }
     }
 }
