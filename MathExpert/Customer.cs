5a86f26681e60dce885585d018539bbb0dc772e6
angelstchavez <angelstchavez@gmail.com>
2023-07-19 01:36:54 -0500

    Agregue la clase Customer

---
+++ MathExpert/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace MathExpert
+{
+    public class Customer
+    {
+        public string GreetCustomer(string firstName, string lastName) 
+            => $"Hello {firstName} {lastName}";
+    }
+}
