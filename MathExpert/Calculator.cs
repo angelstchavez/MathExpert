b37e963459c8f16ed954a28f37dfae98e34a4e3e
angelstchavez <angelstchavez@gmail.com>
2023-07-19 02:22:03 -0500

    Agregar método GetOddRanks a Calculator

--- MathExpert/Calculator.cs
+++ MathExpert/Calculator.cs
@@ -2,10 +2,24 @@
 {
     public class Calculator
     {
+        public List<int> RanksList = new(); 
+
         public int Sum(int x, int y) =>  x + y;
 
         public double SumDouble(double x, double y) => x + y;
 
         public bool IsOdd(int num) => num % 2 != 0;
+
+        public List<int> GetOddRanks(int start, int end)
+        {
+            RanksList.Clear();
+
+            for (int i = start; i < end; i++)
+            {
+                if (i % 2 != 0) RanksList.Add(i); 
+            }
+
+            return RanksList;
+        }
     }
 }
