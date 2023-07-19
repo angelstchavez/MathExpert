42f286b07757b0f5109ea1ca78e71a01f165e601
angelstchavez <angelstchavez@gmail.com>
2023-07-19 01:47:41 -0500

    Agregar CustomerNUnitTests para el método GreetCustomer

---
+++ MathExpert.NUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace MathExpert.NUnitTest
+{
+    [TestFixture]
+    public class CustomerNUnitTests
+    {
+        [Test]
+        public void GreetCustomer_InsertFirstNameLastName_GetGreetFullname()
+        {
+            //Arrage
+            Customer customer = new Customer();
+
+            //Act
+            string result = customer.GreetCustomer("Angel", "Chavez");
+
+            //Assert
+            Assert.That(result, Is.EqualTo("Hello Angel Chavez"));
+        }
+    }
+}
